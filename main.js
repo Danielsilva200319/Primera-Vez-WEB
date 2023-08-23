d020cde243b96c9e48b33eaf71b0aea0fec8d6dd
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com>
2023-08-23 12:42:35 -0500

    actualizar

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-console.log("Hola Mundo")
-console.error("Daniel")
+let array = ["Daniel", "jho", "Manuela","Juan"];
+function saludar(d, f, h, j){
+    console.log(`Hola ${d}`);
+    console.log(`Hola ${f}`);
+    console.log(`Hola ${h}`);
+    console.log(`Hola ${j}`);
+}
+saludar(...array)
